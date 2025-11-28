803b88d6fa218e6ba5fa1d5b3426034f3573ea63
0vGQ0Fw5HEQhy5w4Jy6TRW13E3yL <124201930+A0WTGFjaGecc2k42wP0PZAbQphqh@users.noreply.github.com>
2025-11-29 00:36:42 +0200

    Update loader URL for specific place ID

--- Ix8wZvqICmFbLwNFzxRDgEuYywM7Q0.lua
+++ Ix8wZvqICmFbLwNFzxRDgEuYywM7Q0.lua
@@ -12,7 +12,8 @@ local function HttpGet(Url)
 end
 
 local Places = {
-    [126509999114328] = "https://api.luarmor.net/files/v3/loaders/67321ef983ac3626a0ad87d7aae33ed4.lua"
+    [126371807511901] = "https://api.luarmor.net/files/v3/loaders/6680f99151a097535fe4482d7013a1f3.lua",
+    [126509999114328] = "https://api.luarmor.net/files/v3/loaders/6680f99151a097535fe4482d7013a1f3.lua"
 }
 
 local function Loader()
